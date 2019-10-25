b41baf2a54051c0cc1706735c7d8ebcda0efc914
MA <maning00@hotmail.com>
2019-10-25 09:21:03 +0800

    Update onestep.sh

--- onestep.sh
+++ onestep.sh
@@ -213,14 +213,6 @@ remove_v2ray(){
 
 start_menu(){
     clear
-    green " ===================================="
-    green " 介绍：一键安装v2ray+ws+tls           "
-    green " 系统：centos7                       "
-    green " 作者：atrandys                      "
-    green " 网站：www.atrandys.com              "
-    green " Youtube：atrandys                   "
-    green " ===================================="
-    echo
     green " 1. 安装v2ray+ws+tls"
     green " 2. 升级v2ray"
     red " 3. 卸载v2ray"
